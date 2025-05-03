baf7a55aae33cb33776b81814938d100504db08e
sohithk2002 <sohithkampalli@gmail.com>
2025-05-03 22:54:26 +0530

    hero section

--- app/page.jsx
+++ app/page.jsx
@@ -11,13 +11,13 @@ export default function LandingPage() {
   return (
     <div className="flex flex-col pt-16">
       {/* ───── Hero ───── */}
-      <section className="mt-20 pb-12 space-y-10 md:space-y-20 px-5">
+      <section className="mt-20 pb-12 space-y-10 md:space-y-15 px-5">
         <div className="container mx-auto px-4 md:px-6 text-center space-y-6">
           <Badge variant="outline" className="bg-green-100 text-green-700">
             Split expenses. Simplify life.
           </Badge>
 
-          <h1 className="gradient-title mx-auto max-w-4xl text-4xl font-bold md:text-7xl">
+          <h1 className="gradient-title mx-auto max-w-6xl text-4xl font-bold md:text-8xl">
             The smartest way to split expenses with friends
           </h1>
 
